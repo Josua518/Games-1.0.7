6fd46c3f80db485eda8c32e112f6c656ed692000
Josua518 <84269701+Josua518@users.noreply.github.com>
2021-10-26 12:33:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,117 @@
+$(function() {
+  $( "#button" ).click(function() {
+    $( "#button" ).addClass( "onclic", 250, validate);
+  });
+
+  function validate() {
+    setTimeout(function() {
+      $( "#button" ).removeClass( "onclic" );
+      $( "#button" ).addClass( "validate", 450, callback );
+    }, 2250 );
+  }
+    function callback() {
+      setTimeout(function() {
+        $( "#button" ).removeClass( "validate" );
+      }, 1250 );
+    }
+  });
+  
+ 
+ 
+  $(function() {
+  $( "#button2" ).click(function() {
+    $( "#button2" ).addClass( "onclic", 250, validate);
+  });
+
+  function validate() {
+    setTimeout(function() {
+      $( "#button2" ).removeClass( "onclic" );
+      $( "#button2" ).addClass( "validate", 450, callback );
+    }, 2250 );
+  }
+    function callback() {
+      setTimeout(function() {
+        $( "#button2" ).removeClass( "validate" );
+      }, 1250 );
+    }
+  });
+  
+  
+  $(function() {
+  $( "#button3" ).click(function() {
+    $( "#button3" ).addClass( "onclic", 250, validate);
+  });
+
+  function validate() {
+    setTimeout(function() {
+      $( "#button3" ).removeClass( "onclic" );
+      $( "#button3" ).addClass( "validate", 450, callback );
+    }, 2250 );
+  }
+    function callback() {
+      setTimeout(function() {
+        $( "#button3" ).removeClass( "validate" );
+      }, 1250 );
+    }
+  });
+  
+ 
+ 
+  $(function() {
+  $( "#button4" ).click(function() {
+    $( "#button4" ).addClass( "onclic", 250, validate);
+  });
+
+  function validate() {
+    setTimeout(function() {
+      $( "#button4" ).removeClass( "onclic" );
+      $( "#button4" ).addClass( "validate", 450, callback );
+    }, 2250 );
+  }
+    function callback() {
+      setTimeout(function() {
+        $( "#button4" ).removeClass( "validate" );
+      }, 1250 );
+    }
+  });
+  
+  
+    $(function() {
+  $( "#button5" ).click(function() {
+    $( "#button5" ).addClass( "onclic", 250, validate);
+  });
+
+  function validate() {
+    setTimeout(function() {
+      $( "#button5" ).removeClass( "onclic" );
+      $( "#button3" ).addClass( "validate", 450, callback );
+    }, 2250 );
+  }
+    function callback() {
+      setTimeout(function() {
+        $( "#button5" ).removeClass( "validate" );
+      }, 1250 );
+    }
+  });
+  
+ 
+ 
+  $(function() {
+  $( "#button6" ).click(function() {
+    $( "#button6" ).addClass( "onclic", 250, validate);
+  });
+
+  function validate() {
+    setTimeout(function() {
+      $( "#button6" ).removeClass( "onclic" );
+      $( "#button6" ).addClass( "validate", 450, callback );
+    }, 2250 );
+  }
+    function callback() {
+      setTimeout(function() {
+        $( "#button6" ).removeClass( "validate" );
+      }, 1250 );
+    }
+  });
+  
+  
